2d459ff9a855b9b1e6c58fc6002beb81dbbb6a6f
Saydolimkhon Agzamkhodjaev <saydolimkhon@Saydolimkhons-MacBook-Pro.local>
2020-05-11 17:22:40 -0500

    Added all routes to the site

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var wikiRouter = require('./routes/wiki');
+var catalogRouter = require('./routes/catalog');  
+
 
 var app = express();
 
@@ -32,6 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/wiki', wikiRouter);
+app.use('/catalog', catalogRouter);   
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
