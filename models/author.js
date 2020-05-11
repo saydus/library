eda6e05c84f4d05355c486701acb3eed683f3a8a
Saydolimkhon Agzamkhodjaev <saydolimkhon@Saydolimkhons-MacBook-Pro.local>
2020-05-11 03:32:48 -0500

    Added all DB models

--- models/author.js
+++ models/author.js
@@ -22,11 +22,8 @@ AuthorSchema
   var fullname = '';
   if (this.first_name && this.family_name) {
     fullname = this.family_name + ', ' + this.first_name
-  }
-  if (!this.first_name || !this.family_name) {
-    fullname = '';
-  }
-
+  } 
+  
   return fullname;
 });
 
---
+++ models/genre.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+// Create a Schema for Genre
+var GenreSchema = new Schema(
+ {
+    name: {type: String, required: true, min: 3, max: 100}
+ }
+);
+
+
+// Virtual for the url
+ GenreSchema
+ .virtual('url')
+ .get(function () {
+    return '/genre/' + this.name;
+});
+
+// Export the model
+module.exports = mongoose.model('Genre', GenreSchema);
